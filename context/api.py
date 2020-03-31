f53034edc2b921417168f024267c0b49141f3681
demian-wolf <demianwolfssd@gmail.com>
2020-03-31 15:07:10 +0300

    page_by_page -> pair_by_pair, some headers removed

--- context/api.py
+++ context/api.py
@@ -6,18 +6,8 @@ from bs4 import BeautifulSoup
 
 
 HEADERS = {
-    "Connection": "keep-alive",
-    "Accept": "application/json, text/javascript, */*; q=0.01",
-    "X-Requested-With": "XMLHttpRequest",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
-    "Content-Type": "application/json; charset=UTF-8",
-    "Content-Length": "96",
-    "Origin": "https://context.reverso.net",
-    "Sec-Fetch-Site": "same-origin",
-    "Sec-Fetch-Mode": "cors",
-    "Referer": "https://context.reverso.net/^%^D0^%^BF^%^D0^%^B5^%^D1^%^80^%^D0^%^B5^%^D0^%^B2^%^D0^%^BE^%^D0^%^B4/^%^D0^%^B0^%^D0^%^BD^%^D0^%^B3^%^D0^%^BB^%^D0^%^B8^%^D0^%^B9^%^D1^%^81^%^D0^%^BA^%^D0^%^B8^%^D0^%^B9-^%^D1^%^80^%^D1^%^83^%^D1^%^81^%^D1^%^81^%^D0^%^BA^%^D0^%^B8^%^D0^%^B9/cat",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "User-Agent": "Mozilla/5.0",
+    "Content-Type": "application/json; charset=UTF-8"
 }
 
 class ReversoContextAPI(object):
@@ -38,14 +28,13 @@ class ReversoContextAPI(object):
         data["npage"] = npage
         return requests.post("https://context.reverso.net/bst-query-service", headers=HEADERS, data=json.dumps(data)).json()["list"]
 
-    def get_results_page_by_page(self):
+    def get_results_pair_by_pair(self):
         for npage in range(1, self.page_count + 1):
-            page = self.get_page(npage)
-            for word in page:
+            for word in self.get_page(npage):
                 yield (BeautifulSoup(word["s_text"]).text, BeautifulSoup(word["t_text"]).text)
 
     def get_results(self):
-        return [pair for pair in self.get_results_page_by_page()]
+        return [pair for pair in self.get_results_pair_by_pair()]
         
 if __name__ == "__main__":
     api = ReversoContextAPI(
@@ -54,6 +43,6 @@ if __name__ == "__main__":
            input("Enter the source language code... "),
            input("Enter the target language code... ")
            )
-    results = api.get_results_page_by_page()
+    results = api.get_results_pair_by_pair()
     for pair in results:
         print(pair[0], "=", pair[1])

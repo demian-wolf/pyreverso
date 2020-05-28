6a1af9ff523b7ac67d64ef23d3a3896293755238
Demian Wolf <demianwolfssd@gmail.com>
2020-05-28 15:01:51 +0300

    Reogranized package structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='ReversoAPI',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/demian-wolf/ReversoAPI',
+    license='MIT',
+    author='Demian Wolf',
+    author_email='demianwolfssd@gmail.com',
+    description='Python wraparound for Reverso API'
+)

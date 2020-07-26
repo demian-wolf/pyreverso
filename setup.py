52c07a18361c0bf90b6a1174e046da4fc926088e
demian-wolf <demianwolfssd@gmail.com>
2020-07-26 19:18:23 +0300

    0.0.1.beta.1 release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Reverso_API",
-    version="0.0.1.beta.0",
+    version="0.0.1.beta.1",
     license="MIT",
     author="Demian Wolf",
     author_email="demianwolfssd@gmail.com",
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/demian-wolf/ReversoAPI",
-    download_url="https://github.com/demian-wolf/ReversoAPI/archive/v0.0.1.beta.0.tar.gz",
+    download_url="https://github.com/demian-wolf/ReversoAPI/archive/v0.0.1.beta.1.tar.gz",
     keywords=["REVERSO", "REVERSO-CONTEXT", "REVERSO CONTEXT", "CONTEXT", "REVERSO-VOICE", "REVERSO VOICE", "VOICE",
               "REVERSO-API", "API", "WRAPPER", "PYTHON"],
     packages=setuptools.find_packages(),
